5877858330baa125378082641da98e8555296749
Nathan Puls <71989998+nathanpuls@users.noreply.github.com>
2021-01-15 09:28:20 -0600

    Create googlemp3.js

---
+++ googlemp3.js
@@ -0,0 +1,6 @@
+let googleBaseUrl = https://drive.google.com/file/d/1aJogVh3y3nT13ODdBi_TajgI8zOKuhhZ/view?usp=sharing;
+let audioId = googleBaseUrl.slice(33, 33);
+const baseUrl = https://docs.google.com/uc?export=open&id=;
+const newUrl = baseUrl + audioId;
+
+console.log(newUrl);
